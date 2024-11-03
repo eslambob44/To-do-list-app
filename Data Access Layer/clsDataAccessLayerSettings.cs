f2ed4bd48870249831a3366f79160a131b371755
eslambob44 <eyasser237@gmail.com>
2024-11-03 12:10:36 +0200

    Prepare DataAccessLayer Settings

---
+++ Data Access Layer/clsDataAccessLayerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    static internal class clsDataAccessLayerSettings
+    {
+        static public string _ConnectionString = "Server = . ; DataBase = ToDoListDB ; User ID = sa ; Password = EslamYasse1";
+    }
+}
