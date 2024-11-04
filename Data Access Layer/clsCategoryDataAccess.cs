c24c7d94367c8eee2665d84aa9b61bbf10e6dd59
eslambob44 <eyasser237@gmail.com>
2024-11-04 19:26:39 +0200

    Implement insert category at data access layer

--- Data Access Layer/clsCategoryDataAccess.cs
+++ Data Access Layer/clsCategoryDataAccess.cs
@@ -8,7 +8,36 @@ using System.Data;
 
 namespace Data_Access_Layer
 {
-    internal class clsCategoryDataAccess
+    static public class clsCategoryDataAccess
     {
+        static public int InsertCategory(string CategoryName )
+        {
+            int CategoryID = -1;
+            SqlConnection Connection = new SqlConnection(clsDataAccessLayerSettings._ConnectionString);
+            string Query = @"Insert Into Categories (CategoryName)
+                            values
+                            (@CategoryName);
+                            Select SCOPE_IDENTITY();";
+            SqlCommand Command = new SqlCommand(Query,Connection);
+            Command.Parameters.AddWithValue("@CategoryName", CategoryName);
+            try
+            {
+                Connection.Open();
+                object Result = Command.ExecuteScalar();
+                if(Result!=null && int.TryParse(Result.ToString(),out int TempID))
+                {
+                    CategoryID = TempID;
+                }
+            }
+            catch 
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return CategoryID;
+        }
     }
 }
