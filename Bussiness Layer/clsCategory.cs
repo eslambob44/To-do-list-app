e474e77420b92867e94389fc8825699870bee5c2
eslambob44 <eyasser237@gmail.com>
2024-11-04 21:33:54 +0200

    Implement all details in category class

--- Bussiness Layer/clsCategory.cs
+++ Bussiness Layer/clsCategory.cs
@@ -4,9 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using Data_Access_Layer;
 namespace Bussiness_Layer
 {
-    public class clsCategory
+    static public class clsCategory
     {
+
+        static int CategoryID = -1;
+
+        static public bool AddCategory(string CategoryName)
+        {
+            return clsCategoryDataAccess.InsertCategory(CategoryName) !=-1;
+        }
+
+        static private int _GetCategoryID(string CategoryName)
+        {
+            
+            clsCategoryDataAccess.FindCategoryByName(CategoryName,ref CategoryID);
+            return CategoryID;
+
+        }
+
+        static public bool RemoveCategory(string CategoryName)
+        {
+            return clsCategoryDataAccess.DeleteCategory(_GetCategoryID(CategoryName));
+        }
+
+        static public bool UpdateCategory(string OldCategoryName , string NewCategoryName)
+        {
+            return clsCategoryDataAccess.ModifyCategory(_GetCategoryID(OldCategoryName), NewCategoryName);
+
+        }
+
+        static public bool IsCategoryExists(string CategoryName)
+        {
+            return clsCategoryDataAccess.IsCategoryExists(CategoryName);
+        }
+
+        static public bool IsCategoryExists(int CategoryID)
+        {
+            return clsCategoryDataAccess.IsCategoryExists(CategoryID);
+        }
+
+        static public DataTable ListCategories()
+        {
+            return clsCategoryDataAccess.ListCategories();
+        }
+
+
     }
 }
