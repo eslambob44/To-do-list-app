f7d2d681913311ee05eb161137ac72b4712df913
eslambob44 <eyasser237@gmail.com>
2024-11-05 21:27:43 +0200

    Implement Add-Update form

---
+++ To Do List Disktop App/frmEditUpdateTask.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmAddNewUpdateTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddNewUpdateTask));
+            this.lblAddEdit = new System.Windows.Forms.Label();
+            this.txtTaskName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblDeadLine = new System.Windows.Forms.Label();
+            this.dtpDeadLine = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbCategories = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.chkIsCompleted = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnSave = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAddEdit
+            // 
+            this.lblAddEdit.AutoSize = true;
+            this.lblAddEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddEdit.ForeColor = System.Drawing.Color.White;
+            this.lblAddEdit.Location = new System.Drawing.Point(386, 34);
+            this.lblAddEdit.Name = "lblAddEdit";
+            this.lblAddEdit.Size = new System.Drawing.Size(155, 24);
+            this.lblAddEdit.TabIndex = 6;
+            this.lblAddEdit.Text = "Add/Update Task";
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.AutoRoundedCorners = true;
+            this.txtTaskName.BorderRadius = 13;
+            this.txtTaskName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtTaskName.DefaultText = "";
+            this.txtTaskName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtTaskName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtTaskName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTaskName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTaskName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTaskName.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.txtTaskName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTaskName.Location = new System.Drawing.Point(163, 147);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.PasswordChar = '\0';
+            this.txtTaskName.PlaceholderText = "";
+            this.txtTaskName.SelectedText = "";
+            this.txtTaskName.Size = new System.Drawing.Size(183, 28);
+            this.txtTaskName.TabIndex = 7;
+            this.txtTaskName.Validating += new System.ComponentModel.CancelEventHandler(this.txtTaskName_Validating);
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTask.ForeColor = System.Drawing.Color.White;
+            this.lblTask.Location = new System.Drawing.Point(94, 151);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(50, 24);
+            this.lblTask.TabIndex = 8;
+            this.lblTask.Text = "Task";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.ForeColor = System.Drawing.Color.White;
+            this.lblDescription.Location = new System.Drawing.Point(40, 227);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(104, 24);
+            this.lblDescription.TabIndex = 9;
+            this.lblDescription.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.AutoRoundedCorners = true;
+            this.txtDescription.BorderRadius = 36;
+            this.txtDescription.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDescription.DefaultText = "";
+            this.txtDescription.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDescription.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDescription.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDescription.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDescription.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.txtDescription.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDescription.Location = new System.Drawing.Point(163, 198);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.PasswordChar = '\0';
+            this.txtDescription.PlaceholderText = "Optinal";
+            this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescription.SelectedText = "";
+            this.txtDescription.Size = new System.Drawing.Size(237, 75);
+            this.txtDescription.TabIndex = 10;
+            // 
+            // lblDeadLine
+            // 
+            this.lblDeadLine.AutoSize = true;
+            this.lblDeadLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeadLine.ForeColor = System.Drawing.Color.White;
+            this.lblDeadLine.Location = new System.Drawing.Point(40, 312);
+            this.lblDeadLine.Name = "lblDeadLine";
+            this.lblDeadLine.Size = new System.Drawing.Size(91, 24);
+            this.lblDeadLine.TabIndex = 11;
+            this.lblDeadLine.Text = "DeadLine";
+            // 
+            // dtpDeadLine
+            // 
+            this.dtpDeadLine.Checked = true;
+            this.dtpDeadLine.CustomFormat = "yyyy-MMMM-dd hh:mm tt";
+            this.dtpDeadLine.FillColor = System.Drawing.Color.DarkGray;
+            this.dtpDeadLine.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.dtpDeadLine.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(221)))));
+            this.dtpDeadLine.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDeadLine.Location = new System.Drawing.Point(163, 306);
+            this.dtpDeadLine.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.dtpDeadLine.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.dtpDeadLine.Name = "dtpDeadLine";
+            this.dtpDeadLine.ShowUpDown = true;
+            this.dtpDeadLine.Size = new System.Drawing.Size(224, 36);
+            this.dtpDeadLine.TabIndex = 12;
+            this.dtpDeadLine.Value = new System.DateTime(2024, 11, 5, 20, 40, 17, 88);
+            this.dtpDeadLine.Validating += new System.ComponentModel.CancelEventHandler(this.dtpDeadLine_Validating);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.ForeColor = System.Drawing.Color.White;
+            this.lblCategory.Location = new System.Drawing.Point(40, 377);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(85, 24);
+            this.lblCategory.TabIndex = 13;
+            this.lblCategory.Text = "Category";
+            // 
+            // cbCategories
+            // 
+            this.cbCategories.AutoRoundedCorners = true;
+            this.cbCategories.BackColor = System.Drawing.Color.Transparent;
+            this.cbCategories.BorderRadius = 17;
+            this.cbCategories.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategories.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbCategories.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbCategories.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbCategories.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbCategories.ItemHeight = 30;
+            this.cbCategories.Location = new System.Drawing.Point(184, 371);
+            this.cbCategories.Name = "cbCategories";
+            this.cbCategories.Size = new System.Drawing.Size(162, 36);
+            this.cbCategories.TabIndex = 14;
+            // 
+            // chkIsCompleted
+            // 
+            this.chkIsCompleted.AutoSize = true;
+            this.chkIsCompleted.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.chkIsCompleted.CheckedState.BorderRadius = 0;
+            this.chkIsCompleted.CheckedState.BorderThickness = 0;
+            this.chkIsCompleted.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.chkIsCompleted.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(221)))));
+            this.chkIsCompleted.Location = new System.Drawing.Point(98, 428);
+            this.chkIsCompleted.Name = "chkIsCompleted";
+            this.chkIsCompleted.Size = new System.Drawing.Size(105, 20);
+            this.chkIsCompleted.TabIndex = 15;
+            this.chkIsCompleted.Text = "Is Completed";
+            this.chkIsCompleted.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.chkIsCompleted.UncheckedState.BorderRadius = 0;
+            this.chkIsCompleted.UncheckedState.BorderThickness = 0;
+            this.chkIsCompleted.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnSave
+            // 
+            this.btnSave.AutoRoundedCorners = true;
+            this.btnSave.BorderRadius = 21;
+            this.btnSave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSave.FillColor = System.Drawing.Color.DarkGray;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(338, 437);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(180, 45);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmAddNewUpdateTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(878, 504);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.chkIsCompleted);
+            this.Controls.Add(this.cbCategories);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.dtpDeadLine);
+            this.Controls.Add(this.lblDeadLine);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.lblAddEdit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmAddNewUpdateTask";
+            this.Text = "frmAddNewUpdateTask";
+            this.Load += new System.EventHandler(this.frmAddNewUpdateTask_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddEdit;
+        private Guna.UI2.WinForms.Guna2TextBox txtTaskName;
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.Label lblDescription;
+        private Guna.UI2.WinForms.Guna2TextBox txtDescription;
+        private System.Windows.Forms.Label lblDeadLine;
+        private Guna.UI2.WinForms.Guna2DateTimePicker dtpDeadLine;
+        private System.Windows.Forms.Label lblCategory;
+        private Guna.UI2.WinForms.Guna2ComboBox cbCategories;
+        private Guna.UI2.WinForms.Guna2CheckBox chkIsCompleted;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+    }
+}
---
+++ To Do List Disktop App/frmEditUpdateTask.cs
@@ -0,0 +1,157 @@
+﻿using Bussiness_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmAddNewUpdateTask : Form
+    {
+        clsTask _Task = null;
+        enum enMode { Update = 1 , AddNew=2};
+        enMode _Mode;
+
+        void _CloseForm()
+        {
+            MessageBox.Show("Invalid Task");
+            this.Close();
+        }
+
+        bool _InitialTaskObject(int TaskID)
+        {
+            bool IsTaskCanInitial = true;
+            if (TaskID == -1)
+            {
+                _Task = clsTask.GetAddNewTaskObject();
+                _Mode = enMode.AddNew;
+            }
+            else if (clsTask.IsTaskExists(TaskID))
+            {
+                _Task = clsTask.Find(TaskID);
+                _Mode = enMode.Update;
+            }
+            else 
+                IsTaskCanInitial = false;
+
+            return IsTaskCanInitial;
+
+        }
+        public frmAddNewUpdateTask(int TaskID)
+        {
+            InitializeComponent();
+            
+            if(!_InitialTaskObject(TaskID))
+            {
+                _CloseForm();
+            }
+            
+                
+            
+                
+        }
+
+        void _LoadCategoriesInComboBox()
+        {
+            cbCategories.Items.Clear();
+
+            DataTable dtCategories = clsCategory.ListCategories();
+            foreach (DataRow rowCategory in dtCategories.Rows)
+            {
+                cbCategories.Items.Add(rowCategory["CategoryName"]);
+            }
+        }
+
+        void _LoadForm()
+        {
+            lblAddEdit.Text = ((_Mode == enMode.AddNew)?"Add New":"Update") + " Task";
+            this.Text = lblAddEdit.Text;
+            _LoadCategoriesInComboBox();
+            cbCategories.SelectedIndex = 0;
+            dtpDeadLine.Value = DateTime.Now;
+            if(_Mode  == enMode.Update)
+            {
+                txtTaskName.Text = _Task.TaskName;
+                txtDescription.Text = _Task.TaskDescription;
+                dtpDeadLine.Value = _Task.DeadLine;
+                string Category = clsCategory.GetCategoryName(_Task.CategoryID);
+                cbCategories.SelectedIndex = cbCategories.FindString(Category);
+                chkIsCompleted.Checked = _Task.IsCompleted;
+
+            }
+            
+        }
+
+        private void frmAddNewUpdateTask_Load(object sender, EventArgs e)
+        {
+            _LoadForm();
+        }
+
+        private void txtTaskName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTaskName.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtTaskName, "You must enter task");
+                return;
+            }
+            else if (clsTask.IsTaskExists(txtTaskName.Text))
+            {
+                if (_Mode == enMode.AddNew || (txtTaskName.Text != _Task.TaskName))
+                {
+                    e.Cancel = true;
+                    errorProvider1.SetError(txtTaskName, "There is a task that have similar name");
+                    return;
+                }
+
+            }
+            e.Cancel = false;
+            errorProvider1.SetError(txtTaskName, "");
+            
+        }
+
+        private void dtpDeadLine_Validating(object sender, CancelEventArgs e)
+        {
+            if(_Mode==enMode.Update && dtpDeadLine.Value <= _Task.BeginningDate)
+            {
+                e.Cancel=true;
+                errorProvider1.SetError(dtpDeadLine, "Cant make the dead line before beginning date which is " + _Task.BeginningDate);
+            }
+            else if(_Mode == enMode.AddNew && dtpDeadLine.Value <=DateTime.Now)
+            {
+                e.Cancel=true;
+                errorProvider1.SetError(dtpDeadLine, "Cant make the dead line before now");
+            }
+            else
+            {
+                e.Cancel= false;
+                errorProvider1.SetError(dtpDeadLine, "");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _Task.TaskName = txtTaskName.Text;
+             _Task.TaskDescription = txtDescription.Text;
+            _Task.DeadLine = dtpDeadLine.Value;
+            _Task.CategoryID = clsCategory.GetCategoryID(cbCategories.Text);
+            _Task.IsCompleted = chkIsCompleted.Checked;
+
+            if (_Task.Save())
+            {
+                MessageBox.Show("Task saved successfully");
+                if(_Mode==enMode.AddNew)
+                {
+                    _Mode = enMode.Update;
+                    lblAddEdit.Text = ((_Mode == enMode.AddNew) ? "Add New" : "Update") + " Task";
+                    this.Text = lblAddEdit.Text;
+                }
+            }
+        }
+    }
+}
