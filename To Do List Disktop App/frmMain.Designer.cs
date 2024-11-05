a15a45a75e4570ff62ce92e32815390e079ecf31
eslambob44 <eyasser237@gmail.com>
2024-11-05 15:58:42 +0200

    Update data grid view based on status and name search

--- To Do List Disktop App/frmMain.Designer.cs
+++ To Do List Disktop App/frmMain.Designer.cs
@@ -33,6 +33,11 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.dgvTasks = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.btnShowAll = new Guna.UI2.WinForms.Guna2Button();
+            this.btnShowPending = new Guna.UI2.WinForms.Guna2Button();
+            this.btnShowCompleted = new Guna.UI2.WinForms.Guna2Button();
+            this.txtSearch = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,7 +66,7 @@
             this.dgvTasks.ColumnHeadersHeight = 27;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.Silver;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
             dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
@@ -98,27 +103,121 @@
             this.dgvTasks.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvTasks.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             // 
+            // btnShowAll
+            // 
+            this.btnShowAll.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowAll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowAll.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnShowAll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnShowAll.FillColor = System.Drawing.Color.DarkGray;
+            this.btnShowAll.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.btnShowAll.ForeColor = System.Drawing.Color.White;
+            this.btnShowAll.Location = new System.Drawing.Point(0, 110);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(152, 61);
+            this.btnShowAll.TabIndex = 1;
+            this.btnShowAll.Text = "All";
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnShowPending
+            // 
+            this.btnShowPending.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowPending.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowPending.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnShowPending.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnShowPending.FillColor = System.Drawing.Color.DarkGray;
+            this.btnShowPending.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.btnShowPending.ForeColor = System.Drawing.Color.White;
+            this.btnShowPending.Location = new System.Drawing.Point(0, 166);
+            this.btnShowPending.Name = "btnShowPending";
+            this.btnShowPending.Size = new System.Drawing.Size(152, 61);
+            this.btnShowPending.TabIndex = 2;
+            this.btnShowPending.Tag = "0";
+            this.btnShowPending.Text = "Pending";
+            this.btnShowPending.Click += new System.EventHandler(this.btnShowByStatus_Click);
+            // 
+            // btnShowCompleted
+            // 
+            this.btnShowCompleted.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowCompleted.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowCompleted.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnShowCompleted.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnShowCompleted.FillColor = System.Drawing.Color.DarkGray;
+            this.btnShowCompleted.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.btnShowCompleted.ForeColor = System.Drawing.Color.White;
+            this.btnShowCompleted.Location = new System.Drawing.Point(0, 223);
+            this.btnShowCompleted.Name = "btnShowCompleted";
+            this.btnShowCompleted.Size = new System.Drawing.Size(152, 61);
+            this.btnShowCompleted.TabIndex = 3;
+            this.btnShowCompleted.Tag = "1";
+            this.btnShowCompleted.Text = "Completed";
+            this.btnShowCompleted.Click += new System.EventHandler(this.btnShowByStatus_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.AutoRoundedCorners = true;
+            this.txtSearch.BorderRadius = 13;
+            this.txtSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearch.DefaultText = "";
+            this.txtSearch.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtSearch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtSearch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearch.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearch.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.txtSearch.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearch.Location = new System.Drawing.Point(231, 40);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PasswordChar = '\0';
+            this.txtSearch.PlaceholderText = "Task name";
+            this.txtSearch.SelectedText = "";
+            this.txtSearch.Size = new System.Drawing.Size(183, 28);
+            this.txtSearch.TabIndex = 4;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.ForeColor = System.Drawing.Color.White;
+            this.lblSearch.Location = new System.Drawing.Point(155, 44);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(70, 24);
+            this.lblSearch.TabIndex = 5;
+            this.lblSearch.Text = "Search";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(977, 432);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvTasks);
+            this.Controls.Add(this.btnShowCompleted);
+            this.Controls.Add(this.btnShowPending);
+            this.Controls.Add(this.btnShowAll);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.Text = "To do list";
             this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2DataGridView dgvTasks;
+        private Guna.UI2.WinForms.Guna2Button btnShowAll;
+        private Guna.UI2.WinForms.Guna2Button btnShowPending;
+        private Guna.UI2.WinForms.Guna2Button btnShowCompleted;
+        private Guna.UI2.WinForms.Guna2TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
     }
 }
 
--- To Do List Disktop App/frmMain.cs
+++ To Do List Disktop App/frmMain.cs
@@ -1,4 +1,5 @@
 ﻿using Bussiness_Layer;
+using Guna.UI2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -62,6 +63,54 @@ namespace WindowsFormsApp1
             _ConvertTasksTableIntodgvTable(clsTask.ListTasks());
             _TasksView = _TasksTable.DefaultView;
             _LoadDataGridView();
+            btnShowAll.PerformClick();
+        }
+
+        void _ApplyFilterInDGV()
+        {
+            string Filter = "'%" + txtSearch.Text + "%'";
+            _TasksView.RowFilter = "Name like " + Filter;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            _ApplyFilterInDGV();
+        }
+
+        void _ChangeStatusColor(Guna2Button btn)
+        {
+            btnShowAll.FillColor = Color.DarkGray;
+            btnShowPending.FillColor = Color.DarkGray;
+            btnShowCompleted.FillColor = Color.DarkGray;
+            btn.FillColor = btn.FocusedColor;
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            _ChangeStatusColor(sender as Guna2Button);
+            int CategoryID = 1;
+            DataTable TempTable = clsTask.ListTasksByCategory(1);
+            if (TempTable !=null)
+            {
+                _ConvertTasksTableIntodgvTable(TempTable);
+                dgvTasks.Refresh();
+            }
+        }
+
+
+
+        private void btnShowByStatus_Click(object sender, EventArgs e)
+        {
+            Guna2Button btn = (Guna2Button)sender;
+            _ChangeStatusColor(btn);
+            bool Status = (int.Parse(btn.Tag.ToString()) ==0)?false:true;
+            int CategoryID = 1;
+            DataTable TempTable = clsTask.ListTasksByCategoryAndStatus(1,Status);
+            if (TempTable != null)
+            {
+                 _ConvertTasksTableIntodgvTable(TempTable);
+                dgvTasks.Refresh();
+            }
         }
     }
 }
